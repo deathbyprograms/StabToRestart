edd0e7629ac10717c900035bdd34f86a33533f97
deathbyprograms <DeathByPrograms@gmail.com>
2022-05-22 22:03:55 -0500

    Added configuration and more

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # StabToRestart
+
+A simple BeatSaber mod that allows you to restart a level by committing seppuku.
+
+## How to Install
+
+### Prerequisites
+
+This mod currently requires:
+- BSIPA
+- SiraUtil
+- BeatSaberMarkupLanguage
+
+These requirements can be most easily installed through [ModAssistant](https://github.com/Assistant/ModAssistant/releases/), which is built and maintained by the [BSMG](https://bsmg.wiki/).
+
+### Through the Pre-Compiled .dll
+
+Simply copy the contents of the .zip file in the most recent release in the releases section into the root of your BeatSaber game folder.
+
+### From Source
+
+Import the project into Visual Studio (I believe only VS 2019 or 2022 is supported), and then create a file named `StabToRestart.csproj.user` in the project directory, using the following format
+
+```xml
+<Project ToolsVersion="Current" xmlns="http://schemas.microsoft.com/developer/msbuild/2003">
+  <PropertyGroup>
+    <BeatSaberDir>C:\Path\To\Beat Saber</BeatSaberDir>
+  </PropertyGroup>
+</Project>
+```
+
+If that file is created correctly, references to dependencies will be automatically resolved
+
+## Possible/Known Issues
+
+- I have only tested this on my own headset (a Quest 2), and have been told the system I used to access buttons presses may not work with SteamVR. Note that the button condition may not work for non-Oculus users.
+- When the button condition is not selected, the dropdown menu for the button choice will disappear, while the text will remain.
+
+### Contribute
+
+Any contributions to the source are welcome! Please note that I am new to BeatSaber modding, so it may take awhile for me to verify any pull requests.
--- StabToRestart/StabToRestartController.cs
+++ StabToRestart/StabToRestartController.cs
@@ -1,42 +1,56 @@
 ﻿using UnityEngine;
+using UnityEngine.XR;
+using StabToRestart.Utilities;
+using StabToRestart.Configuration;
+using System;
+using System.Collections.Generic;
 using Zenject;
 
 namespace StabToRestart
 {
     public class StabToRestartController : MonoBehaviour
     {
-        private const float MAX_HORIZONTAL_ANGLE = 20f;
-        private const float MAX_VERTICAL_ANGLE = 10f;
-        private const float MIN_SPEED = 2f;
+        private readonly float MAX_HORIZONTAL_ANGLE = StabToRestartConfig.Instance.MaxHorizontal;
+        private readonly float MAX_VERTICAL_ANGLE = StabToRestartConfig.Instance.MaxVertical;
+        private readonly float MIN_SPEED = 2f;
 
         private float _playerHeight;
         private bool _automaticHeight;
 
         [Inject]
-        private readonly SaberManager _saberManager;
+        private readonly SaberManager _saberManager = null;
+
+        [InjectOptional]
+        private readonly PlayerHeightDetector _phd = null;
 
         [Inject]
-        private readonly PlayerHeightDetector _phd;
+        private readonly MainCamera _mainCamera = null;
+
+        public CapsuleCollider BodyCollider { get; private set; }
 
         [Inject]
-        private readonly MainCamera _mainCamera;
+        private readonly ILevelRestartController _restartController = null;
 
         [Inject]
-        private readonly CapsuleCollider _bodyCollider;
+        private readonly GameplayCoreSceneSetupData _sceneSetupData = null;
 
-        [Inject]
-        private readonly ILevelRestartController _restartController;
+        private InputDevice? _leftController;
 
-        [Inject]
-        private readonly GameplayCoreSceneSetupData _sceneSetupData;
+        private InputDevice? _rightController;
+
+        private float _leftHoldTime { get; set; }
+        private float _rightHoldTime { get; set; }
 
         #region Monobehaviour Messages
 
         private void Start()
         {
+            ControllerGetter.GetControllers(out _leftController, out _rightController);
+
             initPlayerHeight();
 
-            _bodyCollider.transform.localScale = Vector3.one + (Vector3.up * ((_playerHeight / 2) - 1)) - (Vector3.forward * 0.25f);
+            BodyCollider = gameObject.AddComponent<CapsuleCollider>();
+            BodyCollider.transform.localScale = Vector3.one + (Vector3.up * ((_playerHeight / 2) - 1)) - (Vector3.forward * 0.25f);
 
             _saberManager.didUpdateSaberPositionsEvent += HandleSaberMovement;
         }
@@ -46,7 +60,6 @@ namespace StabToRestart
             _restartController.RestartLevel();
         }
 
-
         public void HandleSaberMovement(Saber leftSaber, Saber rightSaber)
         {
             Vector3 leftSaberVector = leftSaber.saberBladeTopPos - leftSaber.saberBladeBottomPos;
@@ -55,20 +68,50 @@ namespace StabToRestart
             float leftSaberLength = leftSaberVector.magnitude;
             float rightSaberLength = rightSaberVector.magnitude;
 
-            if (_bodyCollider.Raycast(new Ray(leftSaber.saberBladeBottomPos, leftSaberVector), out _, leftSaberLength * 0.75f))
+            if (BodyCollider.Raycast(new Ray(leftSaber.saberBladeBottomPos, leftSaberVector), out _, leftSaberLength * 0.75f) && saberCollisionOkay(leftSaber))
             {
-                if (saberCollisionOkay(leftSaber))
+                bool buttonIsPressed;
+                if(StabToRestartConfig.Instance.Mode == StabToRestartConfig.TriggerCondition.Instant)
+                {
+                    restart();
+                }
+                else if ((StabToRestartConfig.Instance.Mode == StabToRestartConfig.TriggerCondition.Button || StabToRestartConfig.Instance.Mode == StabToRestartConfig.TriggerCondition.Both)
+                    && (_leftController.HasValue && _leftController.Value.TryGetFeatureValue(StabToRestartConfig.Instance.SelectedButton, out buttonIsPressed) && buttonIsPressed))
+                {
+                    restart();
+                } 
+                else if((StabToRestartConfig.Instance.Mode == StabToRestartConfig.TriggerCondition.Hold || StabToRestartConfig.Instance.Mode == StabToRestartConfig.TriggerCondition.Both)
+                    && _leftHoldTime > StabToRestartConfig.Instance.MinStabTime)
                 {
                     restart();
                 }
             }
-
-            if (_bodyCollider.Raycast(new Ray(rightSaber.saberBladeBottomPos, rightSaberVector), out _, rightSaberLength))
+            else
             {
-                if (saberCollisionOkay(rightSaber))
+                _leftHoldTime = 0f;
+            }
+
+            if (BodyCollider.Raycast(new Ray(rightSaber.saberBladeBottomPos, rightSaberVector), out _, rightSaberLength * 0.75f) && saberCollisionOkay(rightSaber))
+            {
+                bool buttonIsPressed;
+                if (StabToRestartConfig.Instance.Mode == StabToRestartConfig.TriggerCondition.Instant)
                 {
                     restart();
                 }
+                else if ((StabToRestartConfig.Instance.Mode == StabToRestartConfig.TriggerCondition.Button || StabToRestartConfig.Instance.Mode == StabToRestartConfig.TriggerCondition.Both)
+                    && (_rightController.HasValue && _rightController.Value.TryGetFeatureValue(StabToRestartConfig.Instance.SelectedButton, out buttonIsPressed) && buttonIsPressed))
+                {
+                    restart();
+                }
+                else if ((StabToRestartConfig.Instance.Mode == StabToRestartConfig.TriggerCondition.Hold || StabToRestartConfig.Instance.Mode == StabToRestartConfig.TriggerCondition.Both)
+                    && _rightHoldTime > StabToRestartConfig.Instance.MinStabTime)
+                {
+                    restart();
+                }
+            }
+            else
+            {
+                _rightHoldTime = 0f;
             }
         }
 
@@ -78,7 +121,7 @@ namespace StabToRestart
 
             Vector3 ignoreYVector = Vector3.one - Vector3.up;
             Vector3 onlyYVector = Vector3.up;
-            float horizontalAngle = Vector3.Angle(Vector3.Scale(saberVector, ignoreYVector), Vector3.Scale(_bodyCollider.transform.position - saber.saberBladeBottomPos, ignoreYVector));
+            float horizontalAngle = Vector3.Angle(Vector3.Scale(saberVector, ignoreYVector), Vector3.Scale(BodyCollider.transform.position - saber.saberBladeBottomPos, ignoreYVector));
             float verticalAngle = Vector3.Angle(Vector3.Scale(saberVector, onlyYVector), Vector3.zero);
             float speed = saber.bladeSpeed;
             return horizontalAngle < MAX_HORIZONTAL_ANGLE && verticalAngle < MAX_VERTICAL_ANGLE && speed > MIN_SPEED;
@@ -101,7 +144,7 @@ namespace StabToRestart
         private void playerHeightChanged(float newHeight)
         {
             _playerHeight = newHeight;
-            _bodyCollider.transform.localScale = Vector3.one + (Vector3.up * ((_playerHeight / 2) - 1)) - (Vector3.forward * 0.25f);
+            BodyCollider.transform.localScale = Vector3.one + (Vector3.up * ((_playerHeight / 2) - 1)) - (Vector3.forward * 0.25f);
         }
 
         private void OnDestroy()
@@ -116,8 +159,10 @@ namespace StabToRestart
 
         private void LateUpdate()
         {
-            _bodyCollider.transform.position = _mainCamera.transform.position - (Vector3.up * (_playerHeight / 2));
-            _bodyCollider.transform.Rotate(0, _mainCamera.transform.rotation.eulerAngles.y - _bodyCollider.transform.rotation.eulerAngles.y, 0);
+            _leftHoldTime += Time.deltaTime;
+            _rightHoldTime += Time.deltaTime;
+            BodyCollider.transform.position = _mainCamera.transform.position - (Vector3.up * (_playerHeight / 2));
+            BodyCollider.transform.Rotate(0, _mainCamera.transform.rotation.eulerAngles.y - BodyCollider.transform.rotation.eulerAngles.y, 0);
         }
         #endregion
     }
